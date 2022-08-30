4a54bea4919e4de81dfd68be42d1d459ba5bb375
Rakib Hasan <72100580+rakib8991@users.noreply.github.com>
2022-08-30 12:35:46 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,14 @@
+(function($){
+
+    jQuery(document).ready(function(){
+        
+        jQuery('#ripple').ripples({
+            resolution: 512,
+            dropRadius: 20,
+            perturbance: 0.04,
+        });
+
+    });
+
+
+})(jQuery);
---
+++ assets/js/text-animation.js
@@ -0,0 +1,18 @@
+var typed5 = new Typed('.typed-text', {
+    strings: ['Web Developer', 'Wordpress Expart', 'Youtuber','Freelancer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    cursorChar: '|',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+  });
+  var typed5 = new Typed('.typed-text2', {
+    strings: ['Web Developer', 'Wordpress Expart', 'Youtuber','Freelancer'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    cursorChar: '|',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+  });
